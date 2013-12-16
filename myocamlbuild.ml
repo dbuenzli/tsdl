fa7749b4a5efee92cd5ef683ca4b27dce0bae6a5
Daniel Bünzli <daniel.buenzli@erratique.ch>
2013-12-17 00:50:10 +0100

    Correct -dllib argument.

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -1,6 +1,8 @@
 open Ocamlbuild_plugin
 open Command
 
+(* Generic pkg-config(1) support. *)
+
 let pkg_config flags package = 
   let cmd tmp = 
     Command.execute ~quiet:true & 
@@ -9,20 +11,21 @@ let pkg_config flags package =
   in
   with_temp_file "pkgconfig" "pkg-config" cmd
        
-let pkg_config_lib has_lib lib = 
+let pkg_config_lib has_lib lib stublib = 
   let tag = Printf.sprintf "use_%s" lib in 
   let ocaml_copts pre_opt l = List.map (fun a -> S [A pre_opt; a]) l in
   let cflags = has_lib :: pkg_config "cflags" lib in 
   let cflags_ocaml = ocaml_copts "-ccopt" cflags in
   let ldflags = pkg_config "libs" lib in
   let ldflags_ocaml = ocaml_copts "-cclib" ldflags in
-  let dldflags_ocaml = ocaml_copts "-dllib" ldflags in
+  let dll_stublib = [ A "-dllib"; A (Printf.sprintf "-l%s" stublib) ] in
   flag ["c"; "ocamlmklib"; tag] (S ldflags);
   flag ["c"; "compile"; tag] (S cflags_ocaml); 
   flag ["link"; "ocaml"; tag] (S ((A "-thread") :: ldflags_ocaml));
-  flag ["link"; "ocaml"; "library"; "byte"; tag] (S dldflags_ocaml)
+  flag ["link"; "ocaml"; "library"; "byte"; tag] (S dll_stublib)
     
-    
+(* tsdl_const.ml generation. *)
+
 let sdl_consts_build () =
   dep [ "link"; "ocaml"; "link_consts_stub" ] [ "support/consts_stub.o" ];
   dep [ "sdl_consts" ] [ "src/tsdl_consts.ml" ];
@@ -39,7 +42,7 @@ let sdl_consts_build () =
 let () = 
   dispatch begin function
   | After_rules ->
-      pkg_config_lib (A "-DHAS_SDL2") "sdl2";
+      pkg_config_lib (A "-DHAS_SDL2") "sdl2" "tsdl";
       sdl_consts_build ()
   | _ -> ()
   end
