10c23de64bb553a1ebb53221fec4d024fd2212cc
Daniel Bünzli <daniel.buenzli@erratique.ch>
2013-12-17 04:10:10 +0100

    Correct build flags for linux.

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -3,6 +3,8 @@ open Command
 
 (* Generic pkg-config(1) support. *)
 
+let os = Ocamlbuild_pack.My_unix.run_and_read "uname -s"
+
 let pkg_config flags package = 
   let cmd tmp = 
     Command.execute ~quiet:true & 
@@ -11,17 +13,20 @@ let pkg_config flags package =
   in
   with_temp_file "pkgconfig" "pkg-config" cmd
        
-let pkg_config_lib has_lib lib stublib = 
-  let tag = Printf.sprintf "use_%s" lib in 
+let pkg_config_lib ~lib ~has_lib ~stublib = 
+  let cflags = (A has_lib) :: pkg_config "cflags" lib in
+  let ldflags =
+    let os_flags = if os = "Linux\n" then [ A "-Wl,-no-as-needed"] else [] in
+    os_flags @ pkg_config "libs" lib 
+  in
   let ocaml_copts pre_opt l = List.map (fun a -> S [A pre_opt; a]) l in
-  let cflags = has_lib :: pkg_config "cflags" lib in 
-  let cflags_ocaml = ocaml_copts "-ccopt" cflags in
-  let ldflags = pkg_config "libs" lib in
-  let ldflags_ocaml = ocaml_copts "-cclib" ldflags in
+  let ocaml_cflags = ocaml_copts "-ccopt" cflags in
+  let ocaml_ldflags = ocaml_copts "-cclib" ldflags in
   let dll_stublib = [ A "-dllib"; A (Printf.sprintf "-l%s" stublib) ] in
+  let tag = Printf.sprintf "use_%s" lib in
   flag ["c"; "ocamlmklib"; tag] (S ldflags);
-  flag ["c"; "compile"; tag] (S cflags_ocaml); 
-  flag ["link"; "ocaml"; tag] (S ((A "-thread") :: ldflags_ocaml));
+  flag ["c"; "compile"; tag] (S ocaml_cflags); 
+  flag ["link"; "ocaml"; tag] (S ocaml_ldflags);
   flag ["link"; "ocaml"; "library"; "byte"; tag] (S dll_stublib)
     
 (* tsdl_const.ml generation. *)
@@ -42,7 +47,7 @@ let sdl_consts_build () =
 let () = 
   dispatch begin function
   | After_rules ->
-      pkg_config_lib (A "-DHAS_SDL2") "sdl2" "tsdl";
+      pkg_config_lib ~lib:"sdl2" ~has_lib:"-DHAS_SDL2" ~stublib:"tsdl";
       sdl_consts_build ()
   | _ -> ()
   end
