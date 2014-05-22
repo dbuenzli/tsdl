010b91d887da34f381dd70f1f182915300ff4eee
Daniel Bünzli <daniel.buenzli@erratique.ch>
2014-05-22 23:36:09 +0200

    Fix minimal example in the docs.

--- src/tsdl.mli
+++ src/tsdl.mli
@@ -3426,16 +3426,12 @@ This automatically loads the library and opens the [Tsdl] module.
     with SDL.
 {[
 open Tsdl
-
-let log_err fmt = 
-  let k msg = Sdl.log "%s: %s@." msg (Sdl.get_error ()) in
-  Format.ksprintf k fmt 
     
 let main () = match Sdl.init Sdl.Init.video with 
-| `Error -> log_err "Init error"; exit 1
+| `Error e -> Sdl.log "Init error: %s" e; exit 1
 | `Ok () -> 
     match Sdl.create_window ~w:640 ~h:480 "SDL OpenGL" Sdl.Window.opengl with 
-    | `Error -> log_err "Create window error"; exit 1
+    | `Error e -> Sdl.log "Create window error: %s" e; exit 1
     | `Ok w ->
         Sdl.delay 3000l; 
         Sdl.destroy_window w; 
