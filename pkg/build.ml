3e81dbd8cd25068d4a004ac31fc8c91504b48913
Daniel Bünzli <daniel.buenzli@erratique.ch>
2015-11-29 20:53:08 +0000

    Update topkg and windows compatibility along the way.

--- pkg/build.ml
+++ pkg/build.ml
@@ -3,11 +3,11 @@
 #use "topkg.ml";;
 
 let () =
-  Pkg.describe "tsdl" ~builder:`OCamlbuild [
+  Pkg.describe "tsdl" ~builder:(`OCamlbuild []) [
     Pkg.lib "pkg/META";
     Pkg.lib ~exts:Exts.module_library "src/tsdl";
     Pkg.lib ~exts:Exts.library "src/tsdl_top";
-    Pkg.stublibs "src/dlltsdl.so";
+    Pkg.stublibs ~exts:Exts.c_dll_library "src/dlltsdl";
     Pkg.doc "README.md";
     Pkg.doc "CHANGES.md";
     Pkg.doc "test/min.ml";
