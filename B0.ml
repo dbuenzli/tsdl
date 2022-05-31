ca9149e6b4f22ea091498255fd4e2ed66c27fffe
Daniel Bünzli <daniel.buenzli@erratique.ch>
2022-05-31 16:57:41 +0200

    B0ify: just for the opam file for now.

---
+++ B0.ml
@@ -0,0 +1,44 @@
+open B0_kit.V000
+open B00_std
+
+(* OCaml library names *)
+
+let ctypes = B0_ocaml.libname "ctypes"
+let ctypes_foreign = B0_ocaml.libname "ctypes.foreign"
+let integers = B0_ocaml.libname "integers" (* dep of ctypes *)
+let bigarray_compat = B0_ocaml.libname "bigarray-compat" (* dep of ctypes *)
+let tsdl = B0_ocaml.libname "tsdl"
+
+let compiler_libs_toplevel = B0_ocaml.libname "compiler-libs.toplevel"
+
+(* Packs *)
+
+let default =
+  let meta =
+    let open B0_meta in
+    empty
+    |> tag B0_opam.tag
+    |> add authors ["The tsdl programmers"]
+    |> add maintainers ["Daniel Bünzli <daniel.buenzl i@erratique.ch>"]
+    |> add homepage "https://erratique.ch/software/tsdl"
+    |> add online_doc "https://erratique.ch/software/tsdl/doc/"
+    |> add licenses ["ISC"]
+    |> add repo "git+https://erratique.ch/repos/tsdl.git"
+    |> add issues "https://github.com/dbuenzli/tsdl/issues"
+    |> add description_tags
+      [ "audio"; "bindings"; "graphics"; "media"; "opengl"; "input"; "hci";
+        "org:erratique" ]
+    |> add B0_opam.Meta.depends
+      [ "ocaml", {|>= "4.08.0"|};
+        "ocamlfind", {|build|};
+        "ocamlbuild", {|build|};
+        "topkg", {|build & >= "1.0.3"|};
+        "conf-sdl2", "";
+        "ctypes", {|>= "0.14.0"|};
+        "ctypes-foreign", "";
+      ]
+    |> add B0_opam.Meta.build
+      {|[["ocaml" "pkg/pkg.ml" "build" "--dev-pkg" "%{dev}%"]]|}
+  in
+  B0_pack.v "default" ~doc:"tsdl package" ~meta ~locked:true @@
+  B0_unit.list ()
