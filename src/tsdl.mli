e61b803912415e1d30085e0437dee06d50ea5bee
Daniel Bünzli <daniel.buenzli@erratique.ch>
2023-01-06 10:15:12 +0100

    Fix sample code on latest MacOS (#82).

--- src/tsdl.mli
+++ src/tsdl.mli
@@ -3930,12 +3930,13 @@ This automatically loads the library and opens the [Tsdl] module.
 {[
 open Tsdl
 
-let main () = match Sdl.init Sdl.Init.video with
+let main () = match Sdl.init Sdl.Init.(video + events) with
 | Error (`Msg e) -> Sdl.log "Init error: %s" e; exit 1
 | Ok () ->
     match Sdl.create_window ~w:640 ~h:480 "SDL OpenGL" Sdl.Window.opengl with
     | Error (`Msg e) -> Sdl.log "Create window error: %s" e; exit 1
     | Ok w ->
+        Sdl.pump_events ();
         Sdl.delay 3000l;
         Sdl.destroy_window w;
         Sdl.quit ();
@@ -3946,8 +3947,8 @@ let () = main ()
 
 This can be compiled to byte and native code with:
 {v
-> ocamlfind ocamlc -package tsdl -linkpkg -o min.byte min.ml
-> ocamlfind ocamlopt -package tsdl -linkpkg -o min.native min.ml
+> ocamlfind ocamlc -package tsdl -thread -linkpkg -o min.byte min.ml
+> ocamlfind ocamlopt -package tsdl -thread -linkpkg -o min.native min.ml
 v}
 
 *)
--- test/min.ml
+++ test/min.ml
@@ -1,18 +1,19 @@
 (*
    Minimal OCaml SDL example. This code is in the public domain.
    Compile with:
-   ocamlfind ocamlc -package tsdl -linkpkg -o min.byte min.ml
-   ocamlfind ocamlopt -package tsdl -linkpkg -o min.native min.ml
+   ocamlfind ocamlc -package tsdl -thread -linkpkg -o min.byte min.ml
+   ocamlfind ocamlopt -package tsdl -thread -linkpkg -o min.native min.ml
 *)
 
 open Tsdl
 
-let main () = match Sdl.init Sdl.Init.video with
+let main () = match Sdl.init Sdl.Init.(video + events) with
 | Error (`Msg e) -> Sdl.log "Init error: %s" e; exit 1
 | Ok () ->
     match Sdl.create_window ~w:640 ~h:480 "SDL OpenGL" Sdl.Window.opengl with
     | Error (`Msg e) -> Sdl.log "Create window error: %s" e; exit 1
     | Ok w ->
+        Sdl.pump_events ();
         Sdl.delay 3000l;
         Sdl.destroy_window w;
         Sdl.quit ();
