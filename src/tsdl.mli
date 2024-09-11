29bc9018e7ebdc62cc3c84750bfe97270532dc70
Daniel Bünzli <daniel.buenzli@erratique.ch>
2024-09-11 16:38:22 +0200

    Spdxify.

--- src/tsdl.mli
+++ src/tsdl.mli
@@ -1,6 +1,6 @@
 (*---------------------------------------------------------------------------
    Copyright (c) 2013 The tsdl programmers. All rights reserved.
-   Distributed under the ISC license, see terms at the end of the file.
+   SPDX-License-Identifier: ISC
   ---------------------------------------------------------------------------*)
 
 (** SDL thin bindings.
@@ -3909,19 +3909,3 @@ match Sdl.init Sdl.Init.(video + timer + audio) with
     function and of limited use since most of the time bit fields are
     given to setup state and, as such, are less likley to be used for
     pattern matching. *)
-
-(*---------------------------------------------------------------------------
-   Copyright (c) 2013 The tsdl programmers
-
-   Permission to use, copy, modify, and/or distribute this software for any
-   purpose with or without fee is hereby granted, provided that the above
-   copyright notice and this permission notice appear in all copies.
-
-   THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
-   WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
-   MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
-   ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
-   WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
-   ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
-   OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
-  ---------------------------------------------------------------------------*)
