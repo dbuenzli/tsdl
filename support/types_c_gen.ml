e5de48933dbb3d870d05de76c5e3ef8dc57100c9
Pierre Boutillier <pierre.boutillier@laposte.net>
2025-01-16 14:31:06 +0100

    Hardwired plumbing to generates types using cTypes.stubs

---
+++ support/types_c_gen.ml
@@ -0,0 +1,11 @@
+let c_headers = "#include <SDL.h>"
+
+let main () =
+  let stubs_out = open_out "support/types_stubs_gen.c" in
+  let stubs_fmt = Format.formatter_of_out_channel stubs_out in
+  Format.fprintf stubs_fmt "%s@\n" c_headers;
+  Cstubs.Types.write_c stubs_fmt (module Type_description.Types);
+  Format.pp_print_flush stubs_fmt ();
+  close_out stubs_out
+
+let () = main ()
